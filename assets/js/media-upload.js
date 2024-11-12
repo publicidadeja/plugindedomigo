af4934f8691827c2b1b9d65781889259ff46f7b2
Leonardo Nascimento <144240001+publicidadeja@users.noreply.github.com>
2024-11-11 21:19:29 -0300

    Update media-upload.js

--- assets/js/media-upload.js
+++ assets/js/media-upload.js
@@ -26,3 +26,41 @@ jQuery(document).ready(function($) {
         mediaUploader.open();
     });
 });
+
+$('#tipo_midia').on('change', function() {
+    if ($(this).val() === 'video') {
+        $('#imagem-upload').hide();
+        $('#video-upload').show();
+    } else {
+        $('#imagem-upload').show();
+        $('#video-upload').hide();
+    }
+});
+
+$('#gma-video-upload-btn').on('click', function(e) {
+    e.preventDefault();
+    
+    if (mediaUploader) {
+        mediaUploader.open();
+        return;
+    }
+
+    mediaUploader = wp.media({
+        title: gmaData.wpMediaTitle,
+        button: {
+            text: gmaData.wpMediaButton
+        },
+        library: {
+            type: 'video'
+        },
+        multiple: false
+    });
+
+    mediaUploader.on('select', function() {
+        var attachment = mediaUploader.state().get('selection').first().toJSON();
+        $('#video_url').val(attachment.url);
+        $('#video-preview').html('<video width="320" height="240" controls><source src="' + attachment.url + '" type="' + attachment.mime + '"></video>');
+    });
+
+    mediaUploader.open();
+});
